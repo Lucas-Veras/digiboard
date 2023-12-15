7b1ec4aa0381add6426507e3b4dda1e4b1592254
Lucas-Veras <verasluca00@gmail.com>
2023-12-15 19:53:06 -0300

    feat: add a background

--- src/helpers/canvasHelpers.ts
+++ src/helpers/canvasHelpers.ts
@@ -1,3 +1,5 @@
+import { CANVAS_SIZE } from "@/constants/canvasSize";
+
 export const handleMove = (move: Move, ctx: CanvasRenderingContext2D) => {
   const { path, options } = move;
   const tempCtx = ctx;
@@ -15,12 +17,33 @@ export const handleMove = (move: Move, ctx: CanvasRenderingContext2D) => {
   }
 };
 
+export const drwBackground = (ctx: CanvasRenderingContext2D) => {
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#ccc";
+
+  for (let i = 0; i < CANVAS_SIZE.height; i += 25) {
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(ctx.canvas.width, i);
+    ctx.stroke();
+  }
+
+  for (let i = 0; i < CANVAS_SIZE.width; i += 25) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, ctx.canvas.height);
+    ctx.stroke();
+  }
+};
+
 export const drawAllMoves = (
   ctx: CanvasRenderingContext2D,
   room: ClientRoom,
 ) => {
   const { movesWithoutUser, usersMoves, myMoves } = room;
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  drwBackground(ctx);
 
   movesWithoutUser.forEach((move) => handleMove(move, ctx));
 
